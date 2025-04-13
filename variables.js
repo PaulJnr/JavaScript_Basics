cd86d3e6b055326ebc3876e9111cd1772559e099
Pauljnr <paul.ncube14@gmail.com>
2025-04-13 11:28:44 +0200

    Added index.html, variable.js and modified README.md

---
+++ variables.js
@@ -0,0 +1,26 @@
+// Exercise 1: Variable Declaration
+
+let name = "Zwelinzima Paul Ncube";
+let age = 31;
+let isStudent = false;
+
+console.log("Name:", name, "Age:", age, "Is Student:", isStudent);
+
+// Exercise 2: Variable Reassignment
+
+let favColour = "Tourquoise";
+console.log("My favourite colour is:", favColour);
+
+favColour = "Neon green";
+console.log("My favourite colour is:", favColour);
+
+// Exercise 3: Artithmetic Operations
+
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum, "Difference:", difference, "Product:", product, "Quotient:", quotient);
